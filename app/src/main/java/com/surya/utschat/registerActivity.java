0dc922f2e8888b1181cb994c07baadce8e38fa40
AryaSuryaa <suryasaputra914@gmail.com>
2023-05-06 22:36:13 +0700

    Add Main Activity, sesi Top Bar

--- app/src/main/java/com/surya/utschat/registerActivity.java
+++ app/src/main/java/com/surya/utschat/registerActivity.java
@@ -2,7 +2,10 @@ package com.surya.utschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registerActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class registerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnToOTPActivity = findViewById(R.id.btnToOTP);
+        btnToOTPActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toOTP = new Intent(registerActivity.this, OTPActivity.class);
+                startActivity(toOTP);
+            }
+        });
     }
 }
